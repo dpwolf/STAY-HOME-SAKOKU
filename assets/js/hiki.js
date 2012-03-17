08d6388878d234455993de31d708976c09661d66
David Wolf <dpwolf@mac.com>
2012-03-17 16:30:05 -0400

    added some utility functions for localstorage

--- assets/js/hiki.js
+++ assets/js/hiki.js
@@ -1,3 +1,50 @@
+// global namespace
+hiki = {}
+
+// store some info about the browser
+hiki.info = {};
+hiki.info.supports_local_storage = (function () {
+    try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+    } catch(e) {
+        return false;
+    }
+})();
+
+hiki.utils = {};
+hiki.utils.save_local_param = function( key, value )
+{
+    if(hiki.info.supports_local_storage)
+    {
+        localStorage.setItem( key, value );
+    }
+    else
+    {
+        $.cookie(key, value);
+    }
+};
+hiki.utils.get_local_param = function( key )
+{
+    if (snapr.info.supports_local_storage)
+    {
+        return localStorage.getItem( key );
+    }
+    else
+    {
+        return $.cookie( key );
+    }
+}
+hiki.utils.delete_local_param = function( key )
+{
+    if (hiki.info.supports_local_storage)
+    {
+        localStorage.removeItem( key );
+    } else {
+        $.cookie( key, null );
+    }
+};
+
+
 $(function(){
     hiki_cam = [];
 
